5d94e7c84b02a214c75a1da8a0b4defab086bfde
216120191125 <136864167+216120191125@users.noreply.github.com>
2024-03-01 19:05:54 +0100

    update(folding-menu-animation)

--- src/components/FoldingMenu/index.jsx
+++ src/components/FoldingMenu/index.jsx
@@ -4,18 +4,23 @@ import '../../styles/FoldingMenu/FoldingMenu.css'
 
 function FoldingMenu({title, children}) {
     const [isOpen, setIsOpen] = useState(false)
+    let classNameArrow = "folding-menu-arrow"
+    let classNameMenu = "folding-menu-second-section"
+    let classNameText = "folding-menu-description"
+    if (isOpen) {
+        classNameArrow = "folding-menu-arrow-reverse"
+        classNameMenu = "folding-menu-second-section-slide"
+        classNameText = "folding-menu-description-fade"
+    }
     return (
         <div className="folding-menu">
             <div className='folding-menu-first-section'>
                 <h1 className='folding-menu-title'>{title}</h1>
-                <img className='folding-menu-arrow' src={arrow} alt='flèche pour dérouler le menu' onClick={()=>setIsOpen(!isOpen)}/>
+                <img className={classNameArrow} src={arrow} alt='flèche pour dérouler le menu' onClick={()=>setIsOpen(!isOpen)}/>
             </div>
-            {isOpen && (
-                <div className='folding-menu-second-section'>
-                    <p className='folding-menu-description'>{children}</p>
+                <div className={classNameMenu}>
+                    <p className={classNameText}>{children}</p>
                 </div>
-            )
-        }
         </div>
     )
 } 
--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,20 +1,22 @@
 import fullStar from '../../assets/star-active 1.png'
 import emptyStar from '../../assets/star-inactive 2.png'
+import '../../styles/Rating/Rating.css'
 
 function Rating({rating}) {
     const totlaStar = 5
-    const starNumber = parseInt(rating)
+    const starNumber = new Array(parseInt(rating))
     const emptyStarNumber = totlaStar - starNumber
+    console.log([...Array(starNumber)])
 
     return (
-        <div>
+        <div className='rating'>
             {[...Array(starNumber).map((i) => (
-                <img key={'full-star-' + i} src={fullStar} alt="étoiles pleines"/>
+                <img className='rating-full-star' key={'full-star-' + i} src={fullStar} alt="étoiles pleines"/>
             ))]}
 
-            {[...Array(emptyStarNumber).map((i) => (
-                <img key={'empty-star-' + i} src={emptyStar} alt="étoiles vides"/>
-            ))]}
+            {/* {[...Array(emptyStarNumber).map((i) => (
+                <img className='rating-empty-star' key={'empty-star-' + i} src={emptyStar} alt="étoiles vides"/>
+            ))]} */}
             
         </div>
     )
